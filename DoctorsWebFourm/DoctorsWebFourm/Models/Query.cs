91d54d74af577cf35e7142d3293c76c88a9ec3bb
Umer Abdul Wahid <umerwahiid48@gmail.com>
2023-07-08 20:13:48 +0500

    SignUp

--- DoctorsWebFourm/DoctorsWebFourm/Models/Query.cs
+++ DoctorsWebFourm/DoctorsWebFourm/Models/Query.cs
@@ -3,11 +3,15 @@
     public class Query
     {
         public int QueryId { get; set; }
+
         public int UserId { get; set; }
+        
         public string QueryText { get; set; }
+        
         public DateTime Timestamp { get; set; }
 
         public User User { get; set; }
+        
         public ICollection<Reply> Replies { get; set; }
     }
 }
